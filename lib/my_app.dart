de41f140659e7cb9f8cedd7d32619db04696053c
Ski <skigbprime@gmail.com>
2022-09-07 15:02:32 +0700

    [FIX] resize page when call from main project.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -78,14 +78,9 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         builder: EasyLoading.init(
           builder: (context, child) => ResponsiveWrapper.builder(child,
-              maxWidth: 1200,
-              minWidth: 480,
-              defaultScale: true,
-              breakpoints: [
-                const ResponsiveBreakpoint.resize(480, name: MOBILE),
-                const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-                const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-              ],
+              // maxWidth: 1200,
+              // minWidth: 480,
+              // defaultScale: true,
               background: Container(color: const Color(0xFFF5F5F5))),
         ),
         home: FutureBuilder(
