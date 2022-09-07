388b257397e7c835529102500325b758c3204299
Ski <skigbprime@gmail.com>
2022-09-07 15:33:55 +0700

    [REMOVE] fix phone number.

--- lib/gbkyc.dart
+++ lib/gbkyc.dart
@@ -4,7 +4,7 @@ import 'package:gbkyc/my_app.dart';
 class Gbkyc {
   static Widget register(String phonenumber, {bool isThai = false}) {
     return MyApp(
-      phoneNumber: '0971796690',
+      phoneNumber: phonenumber,
       isThai: isThai,
     );
   }
